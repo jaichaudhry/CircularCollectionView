10e9ba178fa2ef68cbcbe056d90b7a0aca07e59d
Jaycee <jai.chaudhry4@gmail.com>
2015-10-08 23:30:06 +0530

    Initial Commit

---
+++ JCECircularCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCECircularCollectionView
+//
+//  Created by Jai Chaudhry on 08/10/15.
+//  Copyright © 2015 jce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
