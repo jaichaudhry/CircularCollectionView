a50a2b9f3653d0ed16aef966e0edf5ecb8b128b0
Jaycee <jai.chaudhry4@gmail.com>
2015-10-11 21:37:18 +0530

    Revert "Deleting app delegate and main file. As this is not required for framework."
    This reverts commit 1a317f97d65dff7c5af16330cb19f1f55e21c67d.

---
+++ JCECircularCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JCECircularCollectionView
+//
+//  Created by Jai Chaudhry on 08/10/15.
+//  Copyright © 2015 jce. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
