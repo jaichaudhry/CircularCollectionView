8c25ea7e98ffc7a4937f2d46fdda9b9864c322ef
Jaycee <jai.chaudhry4@gmail.com>
2015-11-04 19:47:06 +0530

    Updating view controller according to the datasource.

--- JCECircularCollectionViewExample/JCECircularCollectionViewExample/ViewController.m
+++ JCECircularCollectionViewExample/JCECircularCollectionViewExample/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import <JCECircularCollectionViewController.h>
 
-@interface ViewController ()
+@interface ViewController () <JCECircularCollectionViewControllerDataSource>
 
 @end
 
@@ -17,49 +17,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSArray *titleArray = @[@"First", @"Second", @"Third", @"Fourth"];
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(150, 200, 150, 100)];
-    label.textColor = [UIColor blackColor];
-    label.font = [UIFont systemFontOfSize:12];
-    UIViewController *vc1 = [UIViewController new];
-    vc1.view.backgroundColor = [UIColor redColor];
-    label.text = @"First";
-    [vc1.view addSubview:label];
     
-    UIViewController *vc2 = [UIViewController new];
-    vc2.view.backgroundColor = [UIColor yellowColor];
-    label = [[UILabel alloc] initWithFrame:CGRectMake(150, 200, 150, 100)];
-    label.textColor = [UIColor blackColor];
-    label.font = [UIFont systemFontOfSize:12];
-    label.text = @"Second";
-    [vc2.view addSubview:label];
-    
-    UIViewController *vc3 = [UIViewController new];
-    vc3.view.backgroundColor = [UIColor blueColor];
-    label = [[UILabel alloc] initWithFrame:CGRectMake(150, 200, 150, 100)];
-    label.textColor = [UIColor blackColor];
-    label.font = [UIFont systemFontOfSize:12];
-    label.text = @"Third";
-    [vc3.view addSubview:label];
-    
-    UIViewController *vc4 = [UIViewController new];
-    vc4.view.backgroundColor = [UIColor cyanColor];
-    label = [[UILabel alloc] initWithFrame:CGRectMake(150, 200, 150, 100)];
-    label.textColor = [UIColor blackColor];
-    label.font = [UIFont systemFontOfSize:12];
-    label.text = @"Fourth";
-    [vc4.view addSubview:label];
-    
-    JCECircularCollectionViewController *circularCollectionViewController = [[JCECircularCollectionViewController alloc] initWithTitleArray:titleArray dataViewControllers:@[vc1, vc2, vc3, vc4]];
+    JCECircularCollectionViewController *circularCollectionViewController = [[JCECircularCollectionViewController alloc] init];
+    circularCollectionViewController.datasource = self;
     circularCollectionViewController.showArrow = YES;
     circularCollectionViewController.arrowColor = [UIColor whiteColor];
     [self.navigationController pushViewController:circularCollectionViewController animated:NO];
 //    [self.navigationController presentViewController:circularCollectionViewController animated:NO completion:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - JCECircularCollectionViewControllerDataSource methods
+
+- (NSInteger)numberOfItems {
+    return 4;
+}
+
+- (UIViewController *)circularCollectionViewController:(JCECircularCollectionViewController *)circularCollectionViewController viewControllerForItemAtIndex:(NSInteger)index {
+    return [self p_viewControllerForIndex:index];
+}
+
+- (NSString *)circularCollectionViewController:(JCECircularCollectionViewController *)circularCollectionViewController titleForItemAtIndex:(NSInteger)index {
+    return [self p_titleForIndex:index];
+}
+
+#pragma mark - Private methods
+
+- (NSString *)p_titleForIndex:(NSInteger)index {
+    NSArray *titleArray = @[@"First", @"Second", @"Third", @"Fourth"];
+    return [titleArray objectAtIndex:index];
+}
+
+- (UIViewController *)p_viewControllerForIndex:(NSInteger)index {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(150, 200, 150, 100)];
+    label.textColor = [UIColor blackColor];
+    label.font = [UIFont systemFontOfSize:12];
+    UIViewController *vc = [UIViewController new];
+    vc.view.backgroundColor = [self p_colorForViewControllerAtIndex:index];
+    label.text = [self p_titleForIndex:index];
+    [vc.view addSubview:label];
+    return vc;
+}
+
+- (UIColor *)p_colorForViewControllerAtIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+            return [UIColor redColor];
+        case 1:
+            return [UIColor yellowColor];
+        case 2:
+            return [UIColor blueColor];
+        case 3:
+            return [UIColor greenColor];
+        default:
+            return [UIColor blackColor];
+    }
 }
 
 @end
